ba40c9b55110010648d547b7587fc273f6913d7d
Mary <lg-mary@foxmail.com>
2017-03-16 11:46:16 +0800

    add IDconverter170316.R

--- IDconverter170316.R
+++ IDconverter170316.R
@@ -1,19 +1,30 @@
-rm(list = ls())
+rm(list = ls()) #clear environment values
 
-library("hgu95av2.db")
-probe2entrezID<- toTable(hgu95av2ENTREZID)
-probe2symbol<- toTable(hgu95av2SYMBOL)
-probe2genename<- toTable(hgu95av2GENENAME)
+library("hgu95av2.db") #loading package
 
-my_probe<- sample(unique(mappedLkeys(hgu95av2ENTREZID)),30)
+ls('package:hgu95av2.db') #list package
 
-tmp1 = probe2symbol[match(my_probe,probe2symbol$probe_id),]
-tmp2 = probe2entrezID[match(my_probe,probe2entrezID$probe_id),]
-tmp3 = probe2genename[match(my_probe,probe2genename$probe_id),]
+entrezID<- toTable(hgu95av2ENTREZID)#extract GeneID for probe
+symbol<- toTable(hgu95av2SYMBOL)#extract symbol for probe
+genename<- toTable(hgu95av2GENENAME)#extract genename for probe
 
-write.table(my_probe,'my_probe.txt',quote = F,col.names = F,row.names = F)
-write.table(tmp1$symbol,'my_symbol.txt',quote = F,col.names = F,row.names = F)
-write.table(tmp2$gene_id,'my_geneID.txt',quote = F,col.names = F,row.names = F)
-write.table(tmp3$gene_name,'my_geneName.txt',quote = F,col.names = F,row.names = F)
+probe<- sample(unique(mappedLkeys(hgu95av2ENTREZID)),60)#extract 60samples randomly
 
+tmp1 = symbol[match(probe,symbol$probe_id),]#match probe to genesymbol
+tmp2 = entrezID[match(probe,entrezID$probe_id),]#match probe to geneid
+tmp3 = genename[match(probe,genename$probe_id),]#match probe to genename
+
+gene_ID<- tmp2$gene_id     #show the name for frame
+gene_symbol<- tmp1$symbol
+gene_name<- tmp3$gene_name
+
+write.table(probe,'probe.csv',quote = F,col.names = F,row.names = F,sep="\t")
+write.table(tmp1$symbol,'symbol.csv',quote = F,col.names = F,row.names = F,sep = "\t")
+write.table(tmp2$gene_id,'gene_ID.csv',quote = F,col.names = F,row.names = F,sep = "\t")
+write.table(tmp3$gene_name,'gene_Name.csv',quote = F,col.names = F,row.names = F,sep="\t")
+
+
+df<- data.frame(probe,gene_ID,gene_symbol,gene_name)#build frame
+
+write.table(df,'IDconvert.xls',quote = F,col.names = T,row.names = F,sep = "\t")
 
